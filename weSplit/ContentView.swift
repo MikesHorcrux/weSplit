a7f0dc8ea2f20e2f85d91ad7ccd0b89c2fa47941
Mike Van Amburg <vanamburg.mv@gmail.com>
2019-12-02 23:27:08 -0600

    Initial Commit

---
+++ weSplit/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  weSplit
+//
+//  Created by Mike  Van Amburg on 12/2/19.
+//  Copyright © 2019 Mike Van Amburg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var
+    var body: some View {
+        Text("Hello Mike")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
