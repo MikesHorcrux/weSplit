3a636e4bd84afc20cbef48349e5d8d413ce66db3
Mike Van Amburg <vanamburg.mv@gmail.com>
2019-12-03 12:24:48 -0600

    0.0.2 Mike -Add Navigation tittle

--- weSplit/ContentView.swift
+++ weSplit/ContentView.swift
@@ -29,6 +29,7 @@ struct ContentView: View {
                 Section{
                     Text("$\(checkAmount)")
                 }
+            .navigationBarTitle("weSplit")
             }
     }
     }
